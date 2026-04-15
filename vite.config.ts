5486d49c9c9ce0b3638873fecf447a5b6ed1ed87
omdhuri <omdhuri48@gmail.com>
2026-04-16 02:18:53 +0530

    fix 3

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: mode === 'production' ? '/Skill-Sync/' : '/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     define: {
       'import.meta.env.VITE_GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
